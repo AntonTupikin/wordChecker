45399a9e32bedd9293b49c721e5495c4f6fa5a35
AntonTupikin <antontupikin@icloud.com>
2023-02-24 14:45:38 +0500

    Second

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 public class WordsChecker {
-    String string;
-    String word;
+    protected String string;
+    protected String word;
+    protected Set<String> text = new HashSet<>();
 
 
-    public WordsChecker(String string){
-        this.string=string;
+    public WordsChecker(String string) {
+        this.string = string;
 
     }
 
-    public boolean hasWord(String word){
-        List<String> myList = new ArrayList<String>(Arrays.asList(string.split("\\P{IsAlphabetic}+")));
-
-        return myList.contains(word);
+    public boolean hasWord(String word) {
+        text = new HashSet<>(Arrays.asList(string.split("\\P{IsAlphabetic}+")));
+        return text.contains(word);
     }
 }
